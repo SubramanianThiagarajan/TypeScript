ada722191e2801e2a41c17f2122a67e2d28b9592
SubramanianThiagarajan <subramanian30799@gmail.com>
2021-07-04 01:03:49 +0530

    hello world

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+function sayHello(name) {
+    console.log("Hello," + name);
+}
+sayHello("Subbu");
---
+++ app.ts
@@ -0,0 +1,9 @@
+/*console.log("Hello World");
+*/
+export{};
+
+function sayHello(name: string){
+    console.log("Hello," + name)
+}
+
+sayHello("Subbu")
